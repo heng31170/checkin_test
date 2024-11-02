a95da9992d6bda0912a26b947de2174f26e33361
heng31170 <18655416913@163.com>
2024-11-02 19:08:50 +0800

    第三次提交，完成了条件查询员工请求，但是日期那儿还是不太会，后面康康能不能解决

--- src/main/java/com/zaizi/service/EmpService.java
+++ src/main/java/com/zaizi/service/EmpService.java
@@ -2,6 +2,7 @@ package com.zaizi.service;
 
 import com.zaizi.pojo.Emp;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmpService {
@@ -9,4 +10,6 @@ public interface EmpService {
     List<Emp> getAllEmp();
     // 删除员工
     void delEmp(Integer id);
+    // 条件查询员工
+    List<Emp> getEmp(String name, String gender, LocalDate entryDate);
 }
